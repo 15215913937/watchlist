a02477eb3bbd7e59e3aa4e3a69ccdf7c2dea45be
15215913937 <shenqinan@cnzxa.cn>
2022-03-18 17:30:58 +0800

    add indexpage css(part of the style)

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
+import os
+import sys
+
+import click
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+WIN = sys.platform.startswith('win')
+if WIN:
+    # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:
+    # 否则使用四个斜线
+    prefix = 'sqlite:////'
 app = Flask(__name__)
+# 初始化扩展，传入程序实例 app
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型 修改的监控
+db = SQLAlchemy(app)
+# 创建数据库模型
+# 模型类要声明继承 db.Model
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 名字
+    name = db.Column(db.String(20))
 
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
 @app.route('/')
 @app.route('/index')
 def login():  # put application's code here
@@ -19,7 +49,7 @@ def login():  # put application's code here
         {'title': 'WALL-E', 'year': '2008'},
         {'title': 'The Pork of Music', 'year': '2012'}
     ]
-    return render_template('index.html', name=name,movies = movies)
+    return render_template('index.html', name=name, movies=movies)
 
 
 if __name__ == '__main__':
